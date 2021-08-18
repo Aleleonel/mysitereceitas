9f0fce53dfb59f9c389670441438781c69cfef65
Aleleonel <aleleonel@gmail.com>
2021-08-17 22:17:08 -0300

    validação de campos do formulario de cadastro parte1

--- aplicacao/usuarios/views.py
+++ aplicacao/usuarios/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 
 def cadastro(request):
     template_name = 'usuarios/cadastro.html'
-    return render(request, template_name)
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+        if not nome.strip():
+            print('O Campo nome não pode ser Nulo!')
+            return redirect('cadastro')
+        if not email.strip():
+            print('O Campo email não pode ser Nulo!')
+            return redirect('cadastro')
+
+        if senha != senha2:
+            print("As senha diferem uma da outra")
+            return redirect('cadastro')
+        if User.objects.filter(email=email).exists():
+            print('Usuario já cadatsratrado')
+            return redirect('cadastro')
+        user = User.objects.create_user(username=nome, email=email)
+        # else:
+        #     print(nome, email, senha, senha2)
+        return redirect('login')
+    else:
+        return render(request, template_name)
 
 
 def login(request):
