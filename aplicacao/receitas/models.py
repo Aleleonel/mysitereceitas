5ab718805816f5def23f2d01c900e4f456694987
Aleleonel <aleleonel@gmail.com>
2021-08-22 20:40:03 -0300

    messages em cadatros não funciona

--- aplicacao/receitas/models.py
+++ aplicacao/receitas/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from datetime import datetime
-from pessoas.models import Pessoa
+from django.contrib.auth.models import User
 
 
 class Receita(models.Model):
@@ -13,7 +13,7 @@ class Receita(models.Model):
     data_receita = models.DateTimeField(default=datetime.now, blank=True)
     foto_receita = models.ImageField(upload_to='fotos/%d/%m/%Y', blank=True)
     publicada = models.BooleanField(default=False)
-    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
+    pessoa = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nome_receita
--- aplicacao/usuarios/views.py
+++ aplicacao/usuarios/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from receitas.models import Receita
 
 
 def cadastro(request):
@@ -15,21 +19,19 @@ def cadastro(request):
         if not email.strip():
             print('O Campo email não pode ser Nulo!')
             return redirect('cadastro')
-
         if senha != senha2:
-            print("As senha diferem uma da outra")
+            messages.error(request, 'As senha não são iguais')
             return redirect('cadastro')
         if User.objects.filter(email=email).exists():
-            print('Usuario já cadatsratrado')
+            messages.error(request, 'Usuário já cadastrado')
             return redirect('cadastro')
-
         user = User.objects.create_user(
             username=nome,
             email=email,
             password=senha
             )
         user.save()
-        print('Ususario cadastrado com sucesso')
+        messages.success(request, 'Usuário cadastrado com sucesso')
         return redirect('cadastro')
     else:
         return render(request, template_name)
@@ -37,12 +39,70 @@ def cadastro(request):
 
 def login(request):
     template_name = 'usuarios/login.html'
+    if request.method == 'POST':
+        email = request.POST['email']
+        senha = request.POST['senha']
+        if email == "" or senha == "":
+            print("campos senha e email estão vazios")
+            return redirect('login')
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(email=email).values_list(
+                'username', flat=True).get()
+            user = authenticate(request, username=nome, password=senha)
+            if user is not None:
+                auth.login(request, user)
+                print("Login realizado com sucesso")
+                return redirect('dashboard')
+            else:
+                return redirect('login')
+        else:
+            print(email, senha)
+            print("email e senha inválidos")
+            return redirect('login')
     return render(request, template_name)
 
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect('index')
 
 
 def dashboard(request):
-    pass
+    template_name = 'usuarios/dashboard.html'
+    if request.user.is_authenticated:
+        id = request.user.id
+        receitas = Receita.objects.order_by('-data_receita').filter(pessoa=id)
+        context = {
+            'receitas': receitas
+        }
+        return render(request, template_name, context)
+    else:
+        return redirect('index')
+
+
+def cria_receita(request):
+    template_name = 'usuarios/cria_receita.html'
+    if request.method == 'POST':
+        nome_receita = request.POST['nome_receita']
+        ingredientes = request.POST['ingredientes']
+        modo_preparo = request.POST['modo_preparo']
+        tempo_preparo = request.POST['tempo_preparo']
+        rendimento = request.POST['rendimento']
+        categoria = request.POST['categoria']
+        foto_receita = request.FILES['foto_receita']
+        user = get_object_or_404(User, pk=request.user.id)
+        receita = Receita.objects.create(
+            pessoa=user,
+            nome_receita=nome_receita,
+            ingredientes=ingredientes,
+            modo_preparo=modo_preparo,
+            tempo_preparo=tempo_preparo,
+            rendimento=rendimento,
+            categoria=categoria,
+            foto_receita=foto_receita)
+        receita.save()
+        return redirect('dashboard')
+    else:
+        return render(request, template_name)
+
+    return render(request, template_name)
